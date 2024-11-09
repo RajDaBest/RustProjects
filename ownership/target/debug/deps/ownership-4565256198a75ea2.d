16c3d016924478ba3dbbec980bcfea28979cffe6
RajDaBest <imagination12344321.com@gmail.com>
2024-11-10 05:07:02 +0530

    ownership

---
+++ ownership/target/debug/deps/ownership-4565256198a75ea2.d
@@ -0,0 +1,5 @@
+/home/raj/Desktop/Rust/ownership/target/debug/deps/ownership-4565256198a75ea2: src/main.rs
+
+/home/raj/Desktop/Rust/ownership/target/debug/deps/ownership-4565256198a75ea2.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/raj/Desktop/Rust/ownership/target/debug/ownership: /home/raj/Desktop/Rust/ownership/src/main.rs
